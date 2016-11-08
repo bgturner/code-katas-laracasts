ee359ee9992efece098087dae65cd0d6860ca5b6
Benjamin Turner <benjamin@passionsplay.com>
2016-11-08 12:40:20 -0800

    Add additional tests

--- 01-prime-factors/spec/PrimeFactorsSpec.php
+++ 01-prime-factors/spec/PrimeFactorsSpec.php
@@ -25,4 +25,40 @@ class PrimeFactorsSpec extends ObjectBehavior
 	{
 		$this->generate(4)->shouldReturn([2,2]);
 	}
+
+	function it_should_return_an_array_containing_5_when_passed_5()
+	{
+		$this->generate(5)->shouldReturn([5]);
+	}
+
+	function it_should_return_an_array_containing_2_3_when_passed_6()
+	{
+		$this->generate(6)->shouldReturn([2,3]);
+	}
+
+	function it_should_return_an_array_containing_7_when_passed_7()
+	{
+		$this->generate(7)->shouldReturn([7]);
+	}
+
+	function it_should_return_an_array_containing_2_2_2_when_passed_8()
+	{
+		$this->generate(8)->shouldReturn([2,2,2]);
+	}
+
+	function it_should_return_an_array_containing_3_3_when_passed_9()
+	{
+		$this->generate(9)->shouldReturn([3,3]);
+	}
+
+	function it_should_return_an_array_containing_2_5_when_passed_10()
+	{
+		$this->generate(10)->shouldReturn([2,5]);
+	}
+
+	function it_should_return_an_array_containing_2_2_5_5_when_passed_100()
+	{
+		$this->generate(100)->shouldReturn([2,2,5,5]);
+	}
+
 }
