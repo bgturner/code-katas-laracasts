99657796f8cc42dd3103ec263776007c2f0345c3
Benjamin Turner <benjamin@passionsplay.com>
2016-11-08 11:58:02 -0800

    Add spec for 2

--- 01-prime-factors/spec/PrimeFactorsSpec.php
+++ 01-prime-factors/spec/PrimeFactorsSpec.php
@@ -10,4 +10,9 @@ class PrimeFactorsSpec extends ObjectBehavior
 	function it_should_return_an_empty_array_for_1(){
 		$this->generate(1)->shouldReturn([]);
 	}
+
+	function it_should_return_an_array_containing_2_when_passed_2()
+	{
+		$this->generate(2)->shouldReturn([2]);
+	}
 }
