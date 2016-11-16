21cb3b0d50e3a4c98ef841ad61cfcd9a77d48516
Benjamin Turner <benjamin@passionsplay.com>
2016-11-16 14:15:52 -0800

    Refactor for clarity

--- 03-bowling-game/src/BowlingGame.php
+++ 03-bowling-game/src/BowlingGame.php
@@ -7,6 +7,10 @@ class BowlingGame
 
 	private $rolls = [];
 
+	private $score;
+
+	private $roll;
+
 	public function roll($pins)
 	{
 		$this->rolls[] = $pins;
@@ -14,22 +18,36 @@ class BowlingGame
 
 	public function score()
 	{
-		$score = '';
-		$roll = 0;
+		$this->score = 0;
+		$this->roll = 0;
 
 		for ($frames = 0; $frames < 10; $frames++) {
 
-			if ($this->rolls[$roll] + $this->rolls[$roll + 1] == 10) {
-				$score += $this->rolls[$roll] + $this->rolls[$roll + 1] + $this->rolls[$roll + 2];
+			if ($this->isSpare($this->roll)) {
+				$this->score += $this->scoreSpare($this->roll);
 			} else {
-				$score += $this->rolls[$roll] + $this->rolls[$roll + 1];
+				$this->score += $this->scoreDefaultFrame();
 			}
 
-			$roll += 2;
+			$this->roll += 2;
 
 		}
 
-
-		return $score;
+		return $this->score;
 	}
+
+	public function scoreDefaultFrame()
+	{
+		return $this->rolls[$this->roll] + $this->rolls[$this->roll + 1];
+	}
+
+	private function isSpare($roll) {
+		return ($this->rolls[$roll] + $this->rolls[$roll + 1] == 10) ? true : false;
+	}
+
+	private function scoreSpare($roll)
+	{
+		return $this->rolls[$roll] + $this->rolls[$roll + 1] + $this->rolls[$roll + 2];
+	}
+
 }
