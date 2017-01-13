c4a0a3c4ab82c302d99a7df43803a95c2c10ca99
Benjamin Turner <benjamin@passionsplay.com>
2017-01-12 17:03:22 -0800

    Throws InvalidArgumentException for negative numbers

--- 04-string-calculator/src/StringCalculator.php
+++ 04-string-calculator/src/StringCalculator.php
@@ -8,6 +8,13 @@ class StringCalculator
 	public function add($numbers)
 	{
 		$numbers = explode(',', $numbers);
-		return array_sum($numbers);
+		$total = 0;
+
+		foreach ($numbers as $number) {
+			if ($number < 0) throw new \InvalidArgumentException;
+			$total += $number;
+		}
+
+		return $total;
 	}
 }
